5a8404568aaca9a447d2b37e82728469f6f41e8f
vianney willot <vianney.willot@gmail.com>
2022-08-31 15:03:26 -0400

    finishing the form

--- src/components/EntitiesForm.tsx
+++ src/components/EntitiesForm.tsx
@@ -6,18 +6,19 @@ type UserFormProps = {
 }
 
 export const EntitiesForm = ({setEntity}: UserFormProps) => {
-    const [firstName, setFirstName] = useState<string>();
-    const [lastName, setLastName] = useState<string>();
-    const [street, setStreet] = useState<string>();
-    const [apartment, setApartment] = useState<string>();
-    const [city, setCity] = useState<string>();
+    const [firstName, setFirstName] = useState<string | undefined>();
+    const [lastName, setLastName] = useState<string | undefined>();
+    const [street, setStreet] = useState<string | undefined>();
+    const [apartment, setApartment] = useState<string | undefined>();
+    const [city, setCity] = useState<string | undefined>();
     const [error, setError] = useState(false);
 
     const save = (event: any) => {
-        if (city === undefined || lastName === undefined) {
+        if (city === undefined || city === "" || lastName === undefined || lastName === "") {
             setError(true);
             return
         }
+        setError(false);
         setEntity({
             firstName: firstName,
             lastName: lastName,
@@ -25,6 +26,7 @@ export const EntitiesForm = ({setEntity}: UserFormProps) => {
             apt: apartment,
             city: city
         });
+        setFirstName("");
         setLastName("");
         setStreet("");
         setApartment("");
@@ -46,13 +48,14 @@ export const EntitiesForm = ({setEntity}: UserFormProps) => {
             </label>
             <label className="flex flex-col sm:flex-row">
                 <span className="w-24 font-semibold">Last Name*</span>
-                <input className={`text-blue rounded-md ${error && lastName === undefined ? "ring-4 border-solid ring-red" : ""}`}
-                       type="text"
-                       name={"name"}
-                       value={lastName}
-                       onChange={(event) => {
-                           setLastName(event.target.value)
-                       }}/>
+                <input
+                    className={`text-blue rounded-md ${error && (lastName === undefined || lastName === "") ? "ring-4 border-solid ring-red" : ""}`}
+                    type="text"
+                    name={"name"}
+                    value={lastName}
+                    onChange={(event) => {
+                        setLastName(event.target.value)
+                    }}/>
             </label>
             <label className="flex flex-col sm:flex-row">
                 <span className="w-24 font-semibold">Street</span>
@@ -76,15 +79,19 @@ export const EntitiesForm = ({setEntity}: UserFormProps) => {
             </label>
             <label className="flex flex-col sm:flex-row">
                 <span className="w-24 font-semibold">City*</span>
-                <input className={`text-blue rounded-md ${error && city === undefined ? "ring-4 border-solid ring-red" : ""}`}
-                       type="text"
-                       name={"city"}
-                       value={city}
-                       onChange={(event) => {
-                           setCity(event.target.value)
-                       }}/>
+                <input
+                    className={`text-blue rounded-md ${error && (city === undefined || city === "") ? "ring-4 border-solid ring-red" : ""}`}
+                    type="text"
+                    name={"city"}
+                    value={city}
+                    onChange={(event) => {
+                        setCity(event.target.value)
+                    }}/>
             </label>
-            { error && <p className="font-semibold text-red">Missing field</p>}
+            {error && <p className="font-semibold text-red">Missing field
+                {firstName === "" || firstName === undefined ? " First Name," : ""}
+                {city === "" || city === undefined ? " City" : ""}
+            </p>}
             <section className="flex flex-row justify-center sm:justify-start">
                 <input type="submit" value="Submit"
                        onClick={save}
