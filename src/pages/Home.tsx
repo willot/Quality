7330db8011d134d53aa50d5a604da4314cf97aa9
vianney willot <vianney.willot@gmail.com>
2022-09-08 16:31:31 -0400

    correct bunch of typos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,14 +19,14 @@ export const Home = () => {
         if (textDetails === "button1") {
             return (
                 <section>
-                    Button 1 follow all the requirements. But will a user expect a button to get more information?
+                    Button 1 follows all the requirements, but will a user expect a button to get more information? Should it be a link to a help page?
                 </section>
             )
         }
         if (textDetails === "button2") {
             return (
                 <section>
-                    Button 2 follow all the requirements. But the wording of the button is confusing. Did you checked it
+                    Button 2 follows all the requirements, but the wording of the button is confusing. Did you check it
                     on mobile?
                     Is it too wide?
                 </section>
@@ -35,14 +35,14 @@ export const Home = () => {
         if (textDetails === "button3") {
             return (
                 <section>
-                    Button 3 follow all the requirements. But the color contrast is hard to read.
+                    Button 3 follows all the requirements, but the color contrast is hard to read.
                 </section>
             )
         }
         if (textDetails === "button4") {
             return (
                 <section>
-                    Button 4 fit all the acceptance criteria but it is not accessible... Try to select it by tabbing.
+                    Button 4 fits all the acceptance criteria, but it is not accessible... Try to select it by tabbing.
                 </section>
             )
         }
@@ -106,11 +106,11 @@ export const Home = () => {
             <section className="pb-10">
                 <h2 className="font-bold">Dictionary definition:</h2>
                 <p className="indent-8">The standard of something as measured against other things of a similar kind;
-                    the degree od excellence of something</p>
+                    the degree of excellence of something</p>
                 <p className="text-xs indent-8 pb-5">--Oxford Languages--</p>
-                <p>It is not really useful, doesn't really tell us what is quality. What should I compare my website
-                    too? Do I really want to match my competition if I want to be the best. Is being the best just means
-                    to be the standard for quality?</p>
+                <p>This definition is not really useful. It doesn't really tell us what quality is. What should I compare my website
+                    too? Do I really want to match my competition if I want to be the best? Does being the best mean
+                    being the standard for quality?</p>
             </section>
             <section className="pb-10">
                 <h2 className="font-bold">Useful definition:</h2>
@@ -122,28 +122,29 @@ export const Home = () => {
                 </p>
                 <p className="text-xs indent-8 pb-5">--Joseph Juran--</p>
                 <p>
-                    Basically we produce something the user want and meet their expectation. Quality is all about the
-                    users not about the requirements.
+                    Basically, we produce something the user wants and meet their expectations. Quality is all about the
+                    users, not about the requirements.
                 </p>
                 <p>
-                    As a software engineer this is interesting. The quality of my work is in general base on me
+                    As a software engineer, this definition is interesting. The quality of my work is, in general, based on me
                     completing the requirements of my story, adding tests, passing all the previous tests and someone
-                    validating the work. If I do that my story get accepted and push to prod. But this definition is
-                    interesting because I can do all my work (write tests, pass tests, complete requirements) and
-                    nonetheless produce bad quality if I don't meet the expectation of my users.
+                    validating the work. If I do that, my story gets accepted and pushed to prod. But this definition is
+                    interesting, because I can do all my work (write tests, pass tests, complete requirements) and
+                    nonetheless produce bad quality if I don't meet the expectations of my users.
                 </p>
             </section>
             <section className="pb-10">
                 <h2 className="font-bold">Lets look at this example</h2>
                 <h3>The story:</h3>
                 <section className="border-2 border-white p-2 m-2 rounded-xl">
-                    <p className="pl-2">As a user I want to see more information about quality. Make a button that the
+                    <p className="pl-2">As a user, I want to see more information about quality. Make a button that the
                         user can click to see more information on quality</p>
                     <p className="pl-2 font-semibold">Acceptance criteria:</p>
                     <ul className="pl-4">-Information should appear below the button when clicked</ul>
                     <ul className="pl-4">-Text of the button should change after clicking on it</ul>
-                    <ul className="pl-4">-Information should disappear if the user reclick the button</ul>
+                    <ul className="pl-4">-Information should disappear if the user re-clicks the button</ul>
                 </section>
+                <p>Click on the button below to see how they implement the story:</p>
                 <section className="grid grid-rows-2 grid-flow-col justify-items-center md:grid-rows-1 pt-4">
                     <ExampleButton setTextDetails={setTextDetails} buttonId="button1" buttonWording={buttonText}
                                    css={"bg-pink text-blue w-28 border border-white rounded-md mb-2 active:translate-y-0.5 duration-200 active:bg-white"}/>
@@ -160,17 +161,17 @@ export const Home = () => {
                 </section>
             </section>
             <section className="pb-4">
-                I choose this four examples because they are impossible or hard to write tests for. Has an engineer
-                I can write unit tests, integration tests and end to end tests. I can basically test the logic in the
-                application and make sure the expected behaviors are happening. At least that is what I like to
+                I chose these four examples, because they are impossible or hard to write tests for. As an engineer,
+                I can write unit, integration and end-to-end tests for the ticket acceptance criteria. I can test the basic logic in the
+                application and make sure the expected behaviors are happening. At least, that is what I like to
                 believe...
-                But I can't test if it is what the users expect and I can't test what I didn't think about.
+                But I can't test if it is what the users expect, and I can't test what I didn't think about.
             </section>
             <section className="pt-4">
                 <h2 className="font-bold">Let's look at the tests</h2>
-                <p className="py-2">First test is very simple. Does the text in the button change when the user click on
+                <p className="py-2">The first test is very simple. Does the text in the button change when the user clicks on
                     it? It tests for
-                    the second requirement</p>
+                    the second requirement.</p>
                 <SyntaxHighlighter
                     language='javascript'
                     style={xonokai}
@@ -178,8 +179,8 @@ export const Home = () => {
                 >
                     {test1}
                 </SyntaxHighlighter>
-                <p className="pb-2 pt-4">The second test is slightly more involved, it test the first requirement. Does
-                    extra information appears when the user click on the
+                <p className="pb-2 pt-4">The second test is slightly more involved. It test the first requirement: Does
+                    extra information appears when the user clicks on the
                     button?</p>
                 <SyntaxHighlighter
                     language='javascript'
@@ -188,10 +189,10 @@ export const Home = () => {
                 >
                     {test2}
                 </SyntaxHighlighter>
-                <p className="pb-2 pt-4">The third test keep adding behavioral complexity. If the user click on a button
-                    and see the information and then click on
-                    another button does the text on the first button clicked change correctly? This test is a combination
-                    of all the requirement together</p>
+                <p className="pb-2 pt-4">The third test keeps adding behavioral complexity. If the user clicks on a button
+                    and sees the information and then clicks on
+                    another button, does the text on the first button clicked change correctly? This test is a combination
+                    of all the requirement together.</p>
                 <SyntaxHighlighter
                     language='javascript'
                     style={xonokai}
@@ -199,9 +200,9 @@ export const Home = () => {
                 >
                     {test3}
                 </SyntaxHighlighter>
-                <p className="pb-2 pt-4">The last test is focus on a single button behavior. Does extra information
-                    appears when a button is clicked and then
-                    disappears when the button is clicked again? This is the last requirement</p>
+                <p className="pb-2 pt-4">The last test is focused on a single button behavior: Does extra information
+                    appear when a button is clicked and then
+                    disappear when the button is clicked again? This is the last requirement.</p>
                 <SyntaxHighlighter
                     language='javascript'
                     style={xonokai}
