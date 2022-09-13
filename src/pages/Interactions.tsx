4239b4c40b0072cdfe984c21c5d90376ed066856
vianney willot <vianney.willot@gmail.com>
2022-09-13 17:46:14 -0400

    correct some grammar om the interaction page

--- src/pages/Interactions.tsx
+++ src/pages/Interactions.tsx
@@ -7,7 +7,7 @@ import {LayoutWrapper} from "../components/LayoutWrapper";
 export const Interactions = () => {
     const [buttonIndex, setButtonIndex] = useState<string>("");
 
-    const { pathname } = useLocation();
+    const {pathname} = useLocation();
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
@@ -38,40 +38,33 @@ export const Interactions = () => {
                 <section
                     className="bg-white-bg h-full text-black text-justify py-10 px-1">
                     <h1 className="text-4xl font-medium pb-5 text-pink">Interaction with your application</h1>
-                    <p>You know your application you use it everyday. But do you really know it. Did you ever try to do
-                        things out of order? Did you try to use your keyboard instead of your mouse to navigate? Did you
-                        try to use a screen reader?
-                        When I explore an app I try to think like my kid and be curious. I try to act like I don't know
-                        better.
-                        These arethings you can do to try to change the way you interact with the application you
-                        are exploring.
+                    <p>You know your application. You use it everyday. But, do you really know it? Did you ever try to
+                        do things out of order? Did you try to use your keyboard instead of your mouse to navigate? Did
+                        you try to use a screen reader? When I explore an app I try to think like my kid and be curious.
+                        I try to act like I don't know better. These are things you can do to try to change the way you
+                        interact with the application you are exploring.
                     </p>
                     <section className="py-10">
                         <h2 className="font-bold">Play with the url</h2>
-                        <p>Did you ever try to mess with the url. Click on the link below or better try to mess with the
-                            url.
-                            This is a simple app but sometimes the url contain ids or parameter. It is often a
-                            good thing to try to mess with them and see what happens. See if the application breaks or
-                            if
-                            you get access to things you shouldn't be able to. It is an issue that you can sometimes
-                            find i
-                            applications. The application is properly secure against users that are not logged but when
-                            you
-                            are log in you can sometimes take action you shouldn't be able to because end point is not
-                            properly secure with the right permissions. Messing with a url is a simple way to try
-                            things.
-                            You can also use tools like postman</p>
+                        <p>Did you ever try to mess with the url? Click on the link below or better try to play with the
+                            url of this page. This is a simple app but sometimes the url contains ids or parameters. It
+                            is often a good thing to try to mess with them and see what happens. See if the application
+                            breaks or if you get access to things you shouldn't be able to. It is an issue that you can
+                            sometimes find i applications. The application is properly secure against users that are not
+                            logged in but, when you are logged in you can sometimes take actions you shouldn't be able
+                            to. This happens when endpoints are not properly secure with the right permissions. Messing
+                            with a url is a simple way to try things. You can also use tools like <a
+                                className="text-blue underline underline-offset-2"
+                                href={"https://www.postman.com/"}>postman</a>.</p>
                         <Link to="/blabla" className="flex flex-row items-center mt-2">
                             <span className="text-pink text-lg font-semibold w-52 pl-6">Link to /blabla</span>
                         </Link>
                     </section>
                     <section className="pb-10">
                         <h2 className="font-bold">Use your keyboard</h2>
-                        <p>Lot of people use their keyboard to navigate into applications. Using a
-                            keyboard can have a huge impact on your interaction with an application. These are example
-                            on
-                            what
-                            can go wrong when using a keyboard.</p>
+                        <p>Lot of people use their keyboard to navigate into applications. Using a keyboard can have a
+                            huge impact on your interaction with an application. These are examples of what can go wrong
+                            when using a keyboard.</p>
                         <section className="pt-4">
                             <h3 className="font-medium">Lets play with tabbing. Try to tab through these buttons.</h3>
                             <section
@@ -93,7 +86,7 @@ export const Interactions = () => {
                                     4
                                 </button>
                             </section>
-                            <p>As you can see the ordering of the button is not right. This is a common issue. The
+                            <p>As you can see, the ordering of the buttons is not right. This is a common issue. The
                                 tabbing ordering doesn't follow the expected application flow.</p>
 
                             <p className="pt-6">Let's look at another example. Click or tab on the button you think is
