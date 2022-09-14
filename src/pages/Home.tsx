59def57345004dbfc8ce283bdada9785b6ea61cc
vianney willot <vianney.willot@gmail.com>
2022-09-13 23:03:46 -0400

    fix minor import blocking deployement

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {ExampleButton} from "../components/ExampleButton";
 import {ExampleButtonNotAccessible} from "../components/ExampleButtonNotAccessible";
 
