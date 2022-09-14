a46ef28410e5b313962320b16878cd22ca23b2a2
vianney willot <vianney.willot@gmail.com>
2022-09-13 22:59:41 -0400

    grammar fix for interaction page

--- src/pages/Interactions.tsx
+++ src/pages/Interactions.tsx
@@ -18,18 +18,17 @@ export const Interactions = () => {
             return null;
         }
         if (buttonIndex === "button1") {
-            return "You are incorrect there is no aria label. Someone using a screen reader and that has difficulty to see would orly hear `button` and have" +
-                " no idea what is this button for..."
+            return "You are incorrect, there is no aria-label. Someone using a screen reader would only hear `button` and have no idea what is this button for..."
         }
         if (buttonIndex === "button2") {
-            return "You are correct there is a aria label, the outline is not deactivated and it is button not a div "
+            return "You are correct there is a aria-label, the outline is not deactivated and it is a button not a div."
         }
         if (buttonIndex === "button3") {
             return "You are incorrect the outline of the button is deactivated when the button is focus. So you don't know that" +
                 " you are currently on the button when you tab around"
         }
         if (buttonIndex === "button4") {
-            return "You are incorrect this button is not a button and get ignore by screen reader."
+            return "You are incorrect, this button is not a button and gets ignored by a screen reader."
         }
     }
 
@@ -39,22 +38,22 @@ export const Interactions = () => {
                 <section
                     className="bg-white-bg h-full text-black text-justify py-10 px-1">
                     <h1 className="text-4xl font-medium pb-5 text-pink">Interaction with your application</h1>
-                    <p>You know your application. You use it everyday. But, do you really know it? Did you ever try to
+                    <p>You know your application. You use it every day. But, do you really know it? Did you ever try to
                         do things out of order? Did you try to use your keyboard instead of your mouse to navigate? Did
                         you try to use a screen reader? When I explore an app I try to think like my kid and be curious.
                         I try to act like I don't know better. These are things you can do to try to change the way you
                         interact with the application you are exploring.
                     </p>
                     <section className="py-10">
-                        <h2 className="font-bold">Play with the url</h2>
-                        <p>Did you ever try to mess with the url? Click on the link below or better try to play with the
-                            url of this page. This is a simple app but sometimes the url contains ids or parameters. It
+                        <h2 className="font-bold">Play with the URL</h2>
+                        <p>Did you ever try to mess with the URL? Click on the link below or better try to play with the
+                            URL of this page. This is a simple app but sometimes the URL contains ids or parameters. It
                             is often a good thing to try to mess with them and see what happens. See if the application
                             breaks or if you get access to things you shouldn't be able to. It is an issue that you can
-                            sometimes find i applications. The application is properly secure against users that are not
-                            logged in but, when you are logged in you can sometimes take actions you shouldn't be able
-                            to. This happens when endpoints are not properly secure with the right permissions. Messing
-                            with a url is a simple way to try things. You can also use tools like <a
+                            sometimes find in applications. The application is properly secure against users that are
+                            not logged in but, when you are logged in you can sometimes take actions you shouldn't be
+                            able to. This happens when endpoints are not properly secure with the right permissions.
+                            Messing with a URL is a simple way to try things. You can also use tools like<a
                                 className="text-blue underline underline-offset-2"
                                 href={"https://www.postman.com/"}>postman</a>.</p>
                         <Link to="/blabla" className="flex flex-row items-center mt-2">
@@ -63,11 +62,11 @@ export const Interactions = () => {
                     </section>
                     <section className="pb-10">
                         <h2 className="font-bold">Use your keyboard</h2>
-                        <p>Lot of people use their keyboard to navigate into applications. Using a keyboard can have a
+                        <p>A lot of people use their keyboards to navigate into applications. Using a keyboard can have a
                             huge impact on your interaction with an application. These are examples of what can go wrong
                             when using a keyboard.</p>
                         <section className="pt-4">
-                            <h3 className="font-medium">Lets play with tabbing. Try to tab through these buttons.</h3>
+                            <h3 className="font-medium">Let's play with tabbing. Try to tab through these buttons.</h3>
                             <section
                                 className="grid grid-rows-2 grid-flow-col justify-items-center md:grid-rows-1 pt-4">
                                 <button tabIndex={0}
@@ -125,7 +124,7 @@ export const Interactions = () => {
                                     <ShoppingCartIcon className="h-6 w-6"/>
                                 </div>
                             </section>
-                            <section className="text-green font-semibold">
+                            <section className="text-blue font-semibold">
                                 {buttonMessage()}
                             </section>
 
