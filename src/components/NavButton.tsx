569193f5c4dcb4de9e0d5bfac433fe2b1426d797
vianney willot <vianney.willot@gmail.com>
2022-11-01 15:44:55 -0400

    fix issue with button being too large on very small phone

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -10,10 +10,10 @@ type NavButtonProps = {
 export const NavButton = ({text, linkUrl, left = false}: NavButtonProps) => {
     return (
         <Link to={linkUrl}
-              className="rounded-full bg-pink text-white text-left text-lg mt-4 py-2 px-6 hover:bg-blue flex flex-row">
-            {left && <ArrowLongLeftIcon className="h-8 w-8"/>}
-            <span className=" font-semibold px-4">{text}</span>
-            {!left && <ArrowLongRightIcon className="h-8 w-8"/>}
+              className="rounded-full bg-pink text-white text-left text-sm md:text-lg mt-4 py-2 px-6 hover:bg-blue flex flex-row">
+            {left && <ArrowLongLeftIcon className="h-8 w-8 mr-2 md:mr-4"/>}
+            <span className=" font-semibold my-auto">{text}</span>
+            {!left && <ArrowLongRightIcon className="h-8 w-8 ml-2 md:mr-4"/>}
         </Link>
     )
 }
