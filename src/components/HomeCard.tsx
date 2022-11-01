ff488b4b8c4358efc11d793c2295ee3a8b33558a
vianney willot <vianney.willot@gmail.com>
2022-11-01 11:52:57 -0400

    minor style change to card

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -13,7 +13,7 @@ export const HomeCard = ({title, text, link}: HomeCardProps) => {
         <section className="w-60 mx-4 mt-8 group">
             <Link to={link} className="group flex flex-col justify-between content-between h-full border border-2 border-blue rounded-md transition ease-in-out active:translate-y-0.5 duration-300 active:border-pink hover:border-pink">
                 <div className="space-y-2 p-4 grow">
-                    <h2 className="text-left font-semibold text-blue text-2xl">{title}</h2>
+                    <h2 className="text-left font-semibold text-blue text-2xl transition ease-in-out duration-300 group-hover:text-pink">{title}</h2>
                     <p className="text-left">{text}</p>
                 </div>
                 <div className="border border-t-2 border-x-0 border-b-0 border-t-blue flex flex-row p-4 h-20 grow-0 transition ease-in-out group-active:border-pink group-hover:border-pink duration-300">
