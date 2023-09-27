b1f3f6643cc1e2af5197c29aa891cb1cc66b3c88
vianney willot <vianney.willot@gmail.com>
2023-09-27 13:26:58 -0400

    update summary page with AI

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -26,56 +26,38 @@ export const Summary = () => {
                     <section>
                         <h2 className="font-bold text-2xl border-b border-grey-light pb-2 mb-4" ref={ref1}
                             id="A-useful-tool">A useful tool</h2>
-                        <p className="pb-4">Quality is not only meeting your user's expectations. It is also something
-                            that
-                            makes
-                            you stand out in a good way
-                            or in a bad way. Ultimately to ensure the quality of your product you need to test it. For
-                            that,
-                            you need a testing strategy.</p>
-                        <p className="pb-4">Test automation is a required step in your testing strategy. It is the
-                            foundation
-                            on which you can
-                            build. It gives you confidence that your current application can be released to your users.
-                            Tools like load
-                            testing and usability testing can also be important depending on what is your product. But I
-                            believe one of the most underused tools is
-                            "exploratory testing". Exploratory testing is not writing a script and running it over and
-                            over
-                            before
-                            each release. These kinds of tests NEED to be automated tests. Exploratory testing is where
-                            you
-                            probe
-                            the potential cracks in your application. This lets you find bugs before your users. These
-                            bugs
-                            get
-                            fixed. Unit, integration or end-to-end tests get written to ensure that this bug never
-                            appears
-                            again. Then you move on.</p>
+                        <p className="pb-4">Quality is not solely about meeting user expectations; it's also about
+                            distinguishing your product in a positive or negative light. To ensure a high-quality
+                            product, a well-planned testing strategy is essential.</p>
+                        <p className="pb-4">Test automation plays a pivotal role in your testing strategy, serving as
+                            the foundational pillar on which to build confidence in your application's readiness for
+                            release. Depending on your product, tools like load testing and usability testing can also
+                            be significant. However, one often underutilized tool is "exploratory testing." Unlike
+                            scripted tests, exploratory testing isn't about automating repetitive tasks. Instead, it
+                            involves probing your application for potential weaknesses and vulnerabilities, enabling you
+                            to uncover and address bugs before they impact your users. These discovered issues are then
+                            rectified, and additional automated tests, such as unit, integration, or end-to-end tests,
+                            are developed to prevent their recurrence, allowing your development process to progress
+                            smoothly.</p>
                     </section>
                     <section>
                         <h2 className="pt-10 font-bold text-2xl border-b border-grey-light pb-2 mb-4" ref={ref2}
                             id="When-to-use-exploratory-testing?">When to use exploratory testing?</h2>
-                        <p className="pb-4">I think it is up to each team to decide. For me, exploratory
-                            testing is something that should happen every time concerns are raised about a part of the
-                            application. A lot of teams do not have a QA person but that doesn't mean that you can't run
-                            exploratory testing sessions. The person in charge of accepting stories can use exploratory
-                            testing
-                            to go beyond the simple acceptance criteria. You can also organize weekly sessions and
-                            invite
-                            different team members and guide them to show how exploratory testing let them
-                            discover the limit of their application. </p>
-                        <p>Exploratory testing is a tool. It is not meant to replace other types of testing. It is a
-                            complement to
-                            your
-                            current process. Since it is manual it is often discarded or confused with simple manual
-                            testing. It
-                            is a tool that is worth learning because everybody can gain from it. Engineers getting
-                            better at
-                            testing their own stories before they push them. The PMs, discovering more issues when
-                            accepting
-                            stories.
-                            The product owners gaining an understanding of the system at a deeper level.</p>
+                        <p className="pb-4">The decision of when to employ exploratory testing is ultimately left to
+                            each team's discretion. In my view, exploratory testing should be conducted whenever
+                            concerns arise about a specific aspect of the application. Even if your team lacks a
+                            dedicated QA person, exploratory testing sessions can still be valuable. The individual
+                            responsible for accepting stories can utilize exploratory testing to delve beyond the basic
+                            acceptance criteria. Alternatively, consider organizing weekly sessions and inviting various
+                            team members, guiding them through the process to demonstrate how exploratory testing can
+                            help uncover the limits of the application. </p>
+                        <p>It's important to understand that exploratory testing is a valuable tool intended to
+                            complement, not replace, other forms of testing in your current process. While it is a
+                            manual process, it should not be dismissed or mistaken for simple manual testing. Learning
+                            how to use this tool can benefit everyone involved - engineers can become more adept at
+                            testing their own stories before deployment, PMs can identify more issues during story
+                            acceptance, and product owners can gain a deeper understanding of the system's
+                            intricacies.</p>
                     </section>
                     <section>
                         <h2 className="pt-10 font-bold text-2xl border-b border-grey-light pb-2 mb-4" ref={ref3}
@@ -88,7 +70,8 @@ export const Summary = () => {
                                    href={"https://www.bbc.com/news/world-europe-63499057"}>Article</a>
                             </div>
                             <div>
-                                <p>Last year Microsoft released a version of Microsoft team on Android that blocked calls to 911...
+                                <p>Last year Microsoft released a version of Microsoft team on Android that blocked
+                                    calls to 911...
                                     Again not great for your image</p>
                                 <a className="text-blue underline underline-offset-2"
                                    href={"https://www.androidpolice.com/microsoft-teams-android-911-update/"}>Article</a>
