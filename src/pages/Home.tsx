d6208c4283ac4f9405906b1e659f2674fb777eec
vianney willot <vianney.willot@gmail.com>
2023-09-27 17:30:23 -0400

    rewrite the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,13 +45,13 @@ export const Home = () => {
                 <HamburgerMenuSmallScreen showSideNavBar={showSideNavBar}/>
             </header>
             <section className="m-auto pb-8 md:w-10/12 lg:w-9/12 xl:w-7/12 2xl:w-6/12 grid sm:grid-cols-2 lg:grid-cols-3 justify-items-center sm:justify-items-start">
-                <HomeCard title={"Quality"} text={"This section is a refresher on what quality is"} link={"/quality"}/>
-                <HomeCard title={"Exploratory Testing"} text={"An introduction of what exploratory testing is and the benefits for your application quality."} link={"/exploratoryTesting"}/>
-                <HomeCard title={"Interaction"} text={"This is the first technique to learn to build your exploratory testing focus on changing the way you interact with your application."} link={"/interactions"}/>
-                <HomeCard title={"Objects"} text={"This is the second exploratory testing technique focus on understand how to identify the objects in your application and test them."} link={"/objects"}/>
-                <HomeCard title={"States"} text={"This is the third exploratory testing technique focus on identifying the states in your application."} link={"/states"}/>
-                <HomeCard title={"Environment"} text={"This is the last exploratory testing technique focus on understanding how the environment of your application affect the experience of the users."} link={"/environment"}/>
-                <HomeCard title={"Summary"} text={"Why exploratory testing is a very valuable technique to add to your tool box to improve the quality of your application."} link={"/summary"}/>
+                <HomeCard title={"Quality"} text={"Understanding the Essence of Quality"} link={"/quality"}/>
+                <HomeCard title={"Exploratory Testing"} text={"Unveiling Exploratory Testing and Its Impact on Application Quality"} link={"/exploratoryTesting"}/>
+                <HomeCard title={"Interaction"} text={"Transforming Your Interaction Approach for Enhanced Exploratory Testing"} link={"/interactions"}/>
+                <HomeCard title={"Objects"} text={"Uncovering Objects in Your Application for Effective Exploratory Testing"} link={"/objects"}/>
+                <HomeCard title={"States"} text={"Navigating Through Application States: A Key Aspect of Exploratory Testing"} link={"/states"}/>
+                <HomeCard title={"Environment"} text={"Grasping the Impact of the Application Environment on Exploratory Testing"} link={"/environment"}/>
+                <HomeCard title={"Summary"} text={"The Invaluable Role of Exploratory Testing in Elevating Application Quality"} link={"/summary"}/>
             </section>
             <footer className="bg-blue-light w-full flex flex-row justify-center">
                 <section className="px-4 text-left w-full py-28 md:w-10/12 lg:w-9/12 xl:w-7/12 2xl:w-6/12">
